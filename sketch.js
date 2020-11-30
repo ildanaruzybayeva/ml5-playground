e8c6680b4a634f070b25b5e8804177273f074e53
Ildana <ildana.ruzybayeva@gmail.com>
2020-11-30 17:08:37 +0100

    Predict images from local dir

--- sketch.js
+++ sketch.js
@@ -1,22 +1,31 @@
-let puffin
-let mobilenet
+let mobilenet;
+let puffin;
 
-function modelReady(){
-    console.log("model is ready")
+function modelReady() {
+  console.log("Model is ready!!!");
 }
 
-function imageReady(){
-    image(puffin, 0,0, width, height)
+function imageReady() {
+  image(puffin, 0, 0, width, height);
+  mobilenet.predict(puffin, gotResult);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(results);
+    let label = results[0].className;
+    fill(0);
+    textSize(30);
+    text(label, 10, height - 10);
+  }
 }
 
 function setup() {
   createCanvas(640, 480);
-  puffin = createImg('images/puffin.jpg', imageReady)
-  puffin.hide()
-  background(0)
-  mobilenet = ml5.imageClassifier('MobileNet', modelReady);
-}
-
-function draw() {
+  puffin = createImg("images/penguin.jpg", imageReady);
+  puffin.hide();
   background(0);
+  mobilenet = ml5.imageClassifier("MobileNet", modelReady);
 }
