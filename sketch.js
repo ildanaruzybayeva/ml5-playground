d824232e2509a1888268e89f7c0a29a0ba2e85ef
Ildana <ildana.ruzybayeva@gmail.com>
2020-11-30 15:52:16 +0000

    load images

--- sketch.js
+++ sketch.js
@@ -1,12 +1,20 @@
+let puffin
+let mobilenet
+
 function modelReady(){
     console.log("model is ready")
 }
 
-const mobilenet = ml5.imageClassifier('MobileNet', modelReady);
-
+function imageReady(){
+    image(puffin, 0,0, width, height)
+}
 
 function setup() {
   createCanvas(640, 480);
+  puffin = createImg('images/puffin.jpg', imageReady)
+  puffin.hide()
+  background(0)
+  mobilenet = ml5.imageClassifier('MobileNet', modelReady);
 }
 
 function draw() {
